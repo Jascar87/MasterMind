09a809deae86108d563966ce1c93931f06218025
Jascar87 <riccardopidello@gmail.com>
2017-04-25 15:25:02 +0200

    commit

--- MasterMind/src/Giudice.java
+++ MasterMind/src/Giudice.java
@@ -2,13 +2,22 @@
 public class Giudice {
 	final static int LUNGHEZZA = 4;
 	final static char [] CHARS = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
+	public String target = new String(genTarget());
 	
-	public void genTarget(){
-		
+	public String genTarget(){
+		int[] array = new int[LUNGHEZZA];
+		while(valida(array.toString())==false){
+			for(int i=0; i<LUNGHEZZA; i++){
+				array[i] = (int) (Math.random()*10);
+			}
+		}
+		System.out.println(array.toString());
+		return array.toString();
 	}
 	
 	public static boolean valida(String string) {
 		int stop = 0;
+		if (string==null) return false;
 		if (string.length()!=LUNGHEZZA) return false;
 		for(char cifra: string.toCharArray()){
 			control:
@@ -24,6 +33,28 @@ public class Giudice {
 		
 		return true;
 		
+	}
+
+	public static int numBulls(String string, String string2) {
+		int bulls=0;
+		for(int i=0; i<LUNGHEZZA; i++){
+				if(string.toCharArray()[i]==string2.toCharArray()[i]) bulls++;
+		}
+		return bulls;
+	}
+
+	public static int numMaggots(String string, String string2) {
+		int maggots=0;
+		for(int i=0; i<LUNGHEZZA; i++){
+			for(int j=0; j<LUNGHEZZA; j++){
+				if(i==j) {
+					if(i==LUNGHEZZA-1) return maggots;
+					j++;
+				}
+				if (string.toCharArray()[i]==string2.toCharArray()[j]) maggots++;
+			}
+		}
+		return maggots;
 	}
 
 }
